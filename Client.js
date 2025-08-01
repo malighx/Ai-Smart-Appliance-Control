7d3328c8455c3e6475dde1dc43ec174ba7fbaf6c
Muhammad Ali <maliirfanpk@gmail.com>
2025-08-01 16:33:25 +0500

    Create Client.js

---
+++ Client.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Smart Appliance Control Dashboard</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
